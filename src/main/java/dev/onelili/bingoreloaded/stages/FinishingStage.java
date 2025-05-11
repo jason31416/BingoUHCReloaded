c36cb0e2b0c27041ba70a4e6f2dfdf11d60fb0fb
oneHerobrine <hero_brine@qq.com>
2025-05-11 15:31:51 +0800

    2025/5/11-12:41

--- src/main/java/dev/onelili/bingoreloaded/stages/FinishingStage.java
+++ src/main/java/dev/onelili/bingoreloaded/stages/FinishingStage.java
@@ -1,9 +1,31 @@
 package dev.onelili.bingoreloaded.stages;
 
+import cn.jason31416.planetlib.wrapper.SimplePlayer;
 import dev.onelili.bingoreloaded.games.Arena;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.jetbrains.annotations.NotNull;
 
 public class FinishingStage extends Stage {
     public FinishingStage(Arena arena){
-        super(arena)
+        super(arena);
+    }
+
+    @Override
+    public @NotNull JoinType onJoin(@NotNull SimplePlayer player) {
+        return null;
+    }
+
+    @Override
+    public void perTick() {
+
+    }
+
+    @Override
+    public void init(@NotNull Stage from) {
+
+    }
+    @Override
+    public void end(@NotNull Stage to) {
+
     }
 }
